cd5ad72a18fe5a27915962cb98077fb41ab362ed
syzwina <nor.syazwina.ahmad.fazrul@gmail.com>
2022-06-24 01:35:05 +0800

    Aruco.DICT_6x6_50 is an int, not just some random number

--- src/Main.java
+++ src/Main.java
@@ -11,14 +11,13 @@ import org.opencv.imgcodecs.Imgcodecs;
 public class Main {
     public static void main(String[] args) {
 
-
-
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         Imgcodecs imageCodecs = new Imgcodecs();
         Mat image = imageCodecs.imread("C:\\Users\\norsy\\IdeaProjects\\openCV\\src\\target2.png");
         System.out.println("Image Loaded");
 
-        Dictionary dictionary = Dictionary.create(6,6);
+        // Dictionary dictionary = Dictionary.create(6,6);
+        Dictionary dictionary = Dictionary.get(Aruco.DICT_6X6_50);
         System.out.println("Created Dictionary");
         List<Mat> corners = (List<Mat>) dictionary;
         Mat ids = new Mat();
